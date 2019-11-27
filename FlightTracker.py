80f75fa3e61f8284e7db4c91fe9e0f5047db3d62
jmahajan117 <jay.mahajan33@gmail.com>
2019-11-27 16:22:22 -0500

    Created files for processing flight status requests. Added request file for testing purposes.

---
+++ FlightTracker.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+def main():
+    request = open("FlightRequest.txt", 'r')
+    flight = request.read()
+
+
+if __name__ == "__main__":
+    main()
