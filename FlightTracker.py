eea9e38a3c93077a0c28d99f3e0026e79cded1f0
jmahajan117 <jay.mahajan33@gmail.com>
2019-11-27 22:59:44 -0500

    Found website to web scrap flight info. Allowed for file input from the user.

--- FlightTracker.py
+++ FlightTracker.py
@@ -1,11 +1,21 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import re
 
 
 def main():
     request = open("FlightRequest.txt", 'r')
-    flight = request.read()
+    flight_info = request.read()
+    url = "https://www.flightstats.com/v2/flight-tracker/" + str(flight_info[0:2]) + "/" + str(flight_info[2:])
+    website = urlopen(url)
+
+    parser = BeautifulSoup(website, 'lxml')
+    create_data(parser)
+
+
+def create_data(parser):
+    print(parser.find_all('div', {'class': 'text-helper__TextHelper-s8bko4a-0 blQyiG'}))
 
 
 if __name__ == "__main__":
